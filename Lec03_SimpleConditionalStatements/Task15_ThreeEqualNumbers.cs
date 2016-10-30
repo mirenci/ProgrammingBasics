d42f3ced18cec0266623c670860b32f02648e5ca
mirenci <andrian.angelov@abv.bg>
2016-10-30 17:32:09 +0200

    Task15_ThreeEqualNumbers

---
+++ Lec03_SimpleConditionalStatements/Task15_ThreeEqualNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+ Task 15
+ * Три еднакви числа: да се въведат 3 числа и да се отпечата дали са еднакви (yes / no).
+ */
+class ThreeEqualNumbers
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int thirdNumber = int.Parse(Console.ReadLine());
+
+        if (firstNumber == secondNumber && secondNumber == thirdNumber)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
