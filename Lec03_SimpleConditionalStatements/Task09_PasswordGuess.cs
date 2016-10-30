373eea530d0dc5041591019d67d7a085a42151c5
AndrianAngelov <andrian.angelov@abv.bg>
2016-10-30 15:11:05 +0200

    Task 9 Password Guess

---
+++ Lec03_SimpleConditionalStatements/Task09_PasswordGuess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+//Task 09
+class PasswordGuess
+{
+    static void Main()
+    {
+        string inputValue = Console.ReadLine();
+        string password = "s3cr3t!P@ssw0rd";
+
+        //iterate string by character
+        for (int i = 0; i < inputValue.Length; i++)
+        {
+            if (inputValue[i] != password[i])
+            {
+                Console.WriteLine("Wrong password!");
+                return;
+            }
+            else if (i == inputValue.Length - 1)
+            {
+                Console.WriteLine("Welcome");
+            }
+        }
+    }
+}
