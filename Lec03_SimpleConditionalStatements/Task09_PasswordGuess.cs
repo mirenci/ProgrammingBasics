cedc243ae957347d77790b5a5bd6b64fc3494acf
AndrianAngelov <andrian.angelov@abv.bg>
2016-10-30 15:42:54 +0200

    Simple solution of Task 9

--- Lec03_SimpleConditionalStatements/Task09_PasswordGuess.cs
+++ Lec03_SimpleConditionalStatements/Task09_PasswordGuess.cs
@@ -8,18 +8,27 @@ class PasswordGuess
         string inputValue = Console.ReadLine();
         string password = "s3cr3t!P@ssw0rd";
 
-        //iterate string by character
-        for (int i = 0; i < inputValue.Length; i++)
+        if (inputValue != password)
         {
-            if (inputValue[i] != password[i])
-            {
-                Console.WriteLine("Wrong password!");
-                return;
-            }
-            else if (i == inputValue.Length - 1)
-            {
-                Console.WriteLine("Welcome");
-            }
+            Console.WriteLine("Wrong password!");
         }
+        else
+        {
+            Console.WriteLine("Welcome");
+        }
+
+        ////iterate string by character
+        //for (int i = 0; i < inputValue.Length; i++)
+        //{
+        //    if (inputValue[i] != password[i])
+        //    {
+        //        Console.WriteLine("Wrong password!");
+        //        return;
+        //    }
+        //    else if (i == inputValue.Length - 1)
+        //    {
+        //        Console.WriteLine("Welcome");
+        //    }
+        //}
     }
 }
