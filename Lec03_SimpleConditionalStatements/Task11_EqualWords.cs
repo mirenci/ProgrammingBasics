51241ac7226af50226b68a865ba5b225662bf64b
AndrianAngelov <andrian.angelov@abv.bg>
2016-10-30 15:48:37 +0200

    Task11_EqualWords

---
+++ Lec03_SimpleConditionalStatements/Task11_EqualWords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class EqualWords
+{
+    static void Main()
+    {
+        string firstWord = Console.ReadLine();
+        string secondWord = Console.ReadLine();
+
+        string firstWordLowRegister = firstWord.ToLower();
+        string secondWordLowRegister = secondWord.ToLower();
+
+        if (firstWordLowRegister == secondWordLowRegister)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
