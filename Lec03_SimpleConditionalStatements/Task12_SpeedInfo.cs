c86b494be378aae5ed86fe71f78670b2e85b8229
mirenci <andrian.angelov@abv.bg>
2016-10-30 16:04:54 +0200

    Task12 SpeedInfo

---
+++ Lec03_SimpleConditionalStatements/Task12_SpeedInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+ Task 12
+ Да се напише програма, която въвежда скорост (десетично число) и отпечатва информация за скоростта. 
+ При скорост до 10 (включително) отпечатайте “slow”. 
+ При скорост над 10 и до 50 отпечатайте “average”. При скорост над 50 и до 150 отпечатайте “fast”. 
+ При скорост над 150 и до 1000 отпечатайте “ultra fast”. При по-висока скорост отпечатайте “extremely fast”. 
+ */
+class SpeedInfo
+{
+    static void Main()
+    {
+        float speed = float.Parse(Console.ReadLine());
+
+        if (speed <= 10)
+        {
+            Console.WriteLine("slow");
+        }
+        else if (speed > 10 && speed <= 50)
+        {
+            Console.WriteLine("average");
+        }
+        else if (speed > 50 && speed <= 150)
+        {
+            Console.WriteLine("fast");
+        }
+        else if (speed > 150 && speed <= 1000)
+        {
+            Console.WriteLine("ultra fast");
+        }
+        else
+        {
+            Console.WriteLine("extremely fast");
+        }
+    }
+}
